55bd95bd4cc85fdcc981e223e7c265449f3419f0
MarinaDP2006 <marinadompas@gmail.com>
2025-06-05 09:29:40 +0200

    Update Iterable e Iterator_implements.java

--- Interfaces/Iterable e Iterator_implements.java
+++ Interfaces/Iterable e Iterator_implements.java
@@ -24,3 +24,31 @@ public class Alumno {
 }
 
 /* Utilizamos el iterator() de una clase que ya lo tiene implementado, en este caso, la clase ArrayList */
+
+/* Creamos una clase interna que implementa la interfaz Iterator, y devolvemos una instancia de clase */
+  public class Grupo implements Iterable<Alumno> {
+    private String nombre;
+    private ArrayList<Alumno> alumnos;
+
+  public Grupo(String nombre) {
+    this.nombre = nombre;
+    this.alumnos = new ArrayList<>();
+}
+
+@Override
+public Iterator<Alumno> iterator() {
+  return new IteratorGrupo();
+  }
+
+  private class IteratorGrupo implements Iterator<Alumno> {
+    private int posicion = 0;
+    //métodos abstractos interfaz Iterator<T>
+  public boolean hasNext() { 
+    return posicion < alumnos.size(); 
+  }
+
+    public Alumno next() { 
+      return alumnos.get(posicion++); 
+  }
+ }
+}
