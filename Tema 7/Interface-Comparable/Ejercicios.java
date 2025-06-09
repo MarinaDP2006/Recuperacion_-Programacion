941dce5f274f6a9840975ac78b2e3b2184f20833
MarinaDP2006 <marinadompas@gmail.com>
2025-06-09 09:11:58 +0200

    Create Ejercicios.java

---
+++ Tema 7/Interface-Comparable/Ejercicios.java
@@ -0,0 +1,4 @@
+/* Clase Alumno con nombre y nota. Implementar Comparable para ordenar por nota.
+   Lista de 5 alumnos. Ordenarlos con Collections.sort().
+   Cambiar orden para que sea por nombre en vez de nota.
+*/
