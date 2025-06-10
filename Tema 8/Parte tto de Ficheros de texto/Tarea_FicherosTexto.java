950dda63c0c70ab7272bc0e76d42cc01f3024e45
MarinaDP2006 <marinadompas@gmail.com>
2025-06-10 17:18:57 +0200

    Create Tarea_FicherosTexto.java

---
+++ Tema 8/Parte tto de Ficheros de texto/Tarea_FicherosTexto.java
@@ -0,0 +1,7 @@
+/* 1. Crea un método que reciba un archivo de texto y modiﬁque su contenido, de
+modo que cada palabra del archivo deberá empezar en mayúscula.
+2. Crea un método que permita eliminar todas las ocurrencias de una palabra dada en un
+ﬁchero de texto. Este código producirá automáticamente un nuevo ﬁchero con la 
+siguiente nomenclatura: Si el ﬁchero de entrada se llama ﬁchero.txt, el ﬁchero generado 
+se llamará ﬁchero_2.txt.
+*/
