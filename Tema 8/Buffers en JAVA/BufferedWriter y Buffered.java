e28a4b445fd2c2c921e67db82a56ebabfa0a1b66
MarinaDP2006 <marinadompas@gmail.com>
2025-06-10 10:35:47 +0200

    Update and rename BufferedWriter y Buffered.javaReader to BufferedWriter y Buffered.java

--- Tema 8/Buffers en JAVA/BufferedWriter y Buffered.java
+++ Tema 8/Buffers en JAVA/BufferedWriter y Buffered.java
@@ -1,6 +1,7 @@
 /* 
 ● BufferedReader se utiliza para leer el texto, utiliza un búfer para almacenar datos de entrada.
 Al leer, BufferedReader intentará leer tantos caracteres como sea posible a la vez en el búfer, lo que puede ser mucho más eficiente que leer un carácter a la vez.
+
 ● BufferedWriter se utiliza para escribir texto, utiliza un búfer para almacenar datos de salida.
 Cuando escribes en BufferedWriter, los datos se escriben primero en el búfer y se vacían (escriben) cuando el búfer está lleno o cuando se usa el método flush().
 */
