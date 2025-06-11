0ccbfc8a02df41e1d7d23945991575edd0e16634
MarinaDP2006 <marinadompas@gmail.com>
2025-06-11 13:11:36 +0200

    Update LibroDTO.java

--- Proyecto Biblioteca/DTO/LibroDTO.java
+++ Proyecto Biblioteca/DTO/LibroDTO.java
@@ -1,1 +1,37 @@
+public class LibroDTO {
+    private int id;
+    private String titulo;
+    private String autor;
 
+    // Constructor
+    public LibroDTO(int id, String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
