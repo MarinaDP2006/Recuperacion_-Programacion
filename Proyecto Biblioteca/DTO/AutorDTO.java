2980563b64ad0b4612fa21369aeff78c9a66632e
MarinaDP2006 <marinadompas@gmail.com>
2025-06-11 13:11:19 +0200

    Update AutorDTO.java

--- Proyecto Biblioteca/DTO/AutorDTO.java
+++ Proyecto Biblioteca/DTO/AutorDTO.java
@@ -1,1 +1,27 @@
+public class AutorDTO {
+    private int id;
+    private String nombre;
 
+    // Constructor
+    public AutorDTO(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
