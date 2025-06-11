13943848c79fdb95b29ffc16080c9e1a11f73e26
MarinaDP2006 <marinadompas@gmail.com>
2025-06-11 14:19:37 +0200

    Update Libro.java

--- Proyecto Biblioteca/DAO/Libro.java
+++ Proyecto Biblioteca/DAO/Libro.java
@@ -1,1 +1,22 @@
+public class Libro {
 
+	// INSERTAR LIBRO
+	private void insertarLibro() {
+		
+	}
+	
+	// VER LIBRO
+	private void verLibros() {
+		
+	}
+	
+	// MODIFICAR TITULO DE LIBRO
+	private void modificarTitulo() {
+		
+	}
+	
+	// ELIMINAR LIBRO
+	private void eliminar() {
+		
+	}
+}
