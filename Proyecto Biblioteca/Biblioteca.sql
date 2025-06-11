aff84fcfd8f611238ccc7032ed89eeb3aac0db52
MarinaDP2006 <marinadompas@gmail.com>
2025-06-11 13:26:28 +0200

    Update Biblioteca.sql

--- Proyecto Biblioteca/Biblioteca.sql
+++ Proyecto Biblioteca/Biblioteca.sql
@@ -14,3 +14,15 @@ CREATE TABLE Libro (
     id_autor INT,
     FOREIGN KEY (id_autor) REFERENCES Autor(id_autor) ON DELETE RESTRICT
 );
+
+INSERT INTO Autor (nombre, pais) VALUES ('Stephen King', 'Estados Unidos');
+INSERT INTO Autor (nombre, pais) VALUES ('Colleen Hoover', 'Estados Unidos');
+INSERT INTO Autor (nombre, pais) VALUES ('Gillian Flynn', 'Estados Unidos');
+INSERT INTO Autor (nombre, pais) VALUES ('J.K. Rowling', 'Reino Unido');
+INSERT INTO Autor (nombre, pais) VALUES ('Neil Gaiman', 'Reino Unido');
+
+INSERT INTO Libro (titulo, anio, id_autor) VALUES ('The Boogeyman', 2025, 1); 
+INSERT INTO Libro (titulo, anio, id_autor) VALUES ('It Ends With Us', 2024, 2); 
+INSERT INTO Libro (titulo, anio, id_autor) VALUES ('Gone Girl', 2024, 3); 
+INSERT INTO Libro (titulo, anio, id_autor) VALUES ('Fantastic Beasts: The Secrets of Dumbledore', 2025, 4); 
+INSERT INTO Libro (titulo, anio, id_autor) VALUES ('The Sandman', 2024, 5);
