e58bf813a214823626537310bc23ba6cafb3ace1
MarinaDP2006 <marinadompas@gmail.com>
2025-06-12 10:38:40 +0200

    Update AppBiblioteca.java

--- Proyecto Biblioteca/main/AppBiblioteca.java
+++ Proyecto Biblioteca/main/AppBiblioteca.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import DAO.Autor;
+import DAO.Libro;
 
 public class AppBiblioteca {
     public static void main(String[] args) {
@@ -16,28 +18,53 @@ public class AppBiblioteca {
             System.out.println("5. Modificar título de un libro");
             System.out.println("6. Eliminar un libro");
             System.out.println("0. Salir");
-            System.out.print("Seleccione opción: ");
+            System.out.print("\nSeleccione opción: ");
             opcion = sc.nextInt();
+            System.out.println();
             sc.nextLine();
 
             switch (opcion) {
                 case 1:
-                    // Insertar autor
-                    break;
+                	// Insertar autor
+					System.out.print("Ingrese el nombre del autor: ");
+					String nombreAutor = sc.nextLine();
+					System.out.print("Ingrese el país del autor: ");
+					String paisAutor = sc.nextLine();
+					autor.insertarAutor(nombreAutor, paisAutor);
+                	break;
                 case 2:
                     // Ver autores
+                	autor.verAutores();
                     break;
                 case 3:
                     // Insertar libro
+                System.out.print("Ingrese el título del libro: ");
+                String tituloLibro = sc.nextLine();
+                System.out.print("Ingrese el año de publicación: ");
+                int anioLibro = sc.nextInt();
+                System.out.print("Ingrese el ID del autor: ");
+                int idAutorLibro = sc.nextInt();
+                sc.nextLine(); 
+                libro.insertarLibro(tituloLibro, anioLibro, idAutorLibro);
                     break;
                 case 4:
                     // Ver libros
+                	libro.verLibros();
                     break;
                 case 5:
                     // Modificar título de libro
+                System.out.print("Ingrese el ID del libro a modificar: ");
+                int idLibro = sc.nextInt();
+                sc.nextLine();
+                System.out.print("Ingrese el nuevo título del libro: ");
+                String nuevoTitulo = sc.nextLine();
+                libro.modificarTitulo(idLibro, nuevoTitulo);
                     break;
                 case 6:
                     // Eliminar libro
+                	System.out.print("Ingrese el ID del libro a eliminar: ");
+                	int idLibroEliminar = sc.nextInt();
+                	libro.eliminar(idLibroEliminar);
                     break;
                 case 0:
                     System.out.println("Saliendo...");
