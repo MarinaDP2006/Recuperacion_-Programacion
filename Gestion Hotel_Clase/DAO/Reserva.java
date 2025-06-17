4e4a9cf772bf85042a13671eb9c772282664f634
MarinaDP2006 <marinadompas@gmail.com>
2025-06-17 09:32:44 +0200

    Update Reserva.java

--- Gestion Hotel_Clase/DAO/Reserva.java
+++ Gestion Hotel_Clase/DAO/Reserva.java
@@ -1,1 +1,51 @@
+package DTO;
+import java.sql.Date;
 
+public class Reserva {
+    private int idReserva;
+    private int idCliente;
+    private int idHabitacion;
+    private Date fechaEntrada;
+    private Date fechaSalida;
+
+    // Getters y Setters
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
